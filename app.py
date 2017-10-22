0273af6d8fd4357032ec704245b12e5890fefa59
pixielf <30912100+pixielf@users.noreply.github.com>
2017-10-22 05:33:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -63,111 +63,6 @@ def result():
 		kwargs = different_buildings(start_bldg, end_bldg, start_label, end_label)
 
 	return render_template('result.html', **kwargs)
-	# if start_bldg == end_bldg:
-	# 	# just have to deal with one building
-	# 	name = translation[start_bldg]['building_name']
-	# 	data = os.path.join('maps', 'map_data', translation[start_bldg]['map_data'])
-	# 	graph = Graph.from_file(data)
-	# 	start = graph.get_from_label(session['start-room'])
-	# 	end = graph.get_from_label(session['end-room'])
-	#
-	# 	if not start or not end:
-	# 		flash('Error. Label not found.')
-	# 		return redirect(url_for('index'))
-	#
-	# 	cost, path = graph.dijkstra(start, end)
-	# 	t = session['time_']
-	#
-	# 	if start.z == end.z:
-	# 		# same floor
-	# 		bw_map = translation[start_bldg]['bw_maps'][str(start.z)]
-	# 		filename = os.path.join('static', f'{bw_map}_{t}.png')
-	# 		dynamic_image = genimg2D(os.path.join('maps', 'bw_maps', bw_map), path)
-	# 		dynamic_image.save(filename)
-	#
-	# 		imgs = [filename]
-	# 	else:
-	# 		imgs = []
-	#
-	# 		# starting floor
-	# 		path1 = list(filter(lambda n: n.z == start.z, path))
-	# 		path2 = list(filter(lambda n: n.z == end.z, path))
-	#
-	# 		map1 = translation[start_bldg]['bw_maps'][str(start.z)]
-	# 		map2 = translation[start_bldg]['bw_maps'][str(end.z)]
-	#
-	# 		filename1 = os.path.join('static', f'{map1}_{t}.png')
-	# 		filename2 = os.path.join('static', f'{map2}_{t}.png')
-	#
-	# 		imgs = genimg3D(
-	# 			os.path.join('maps', 'bw_maps', map1),
-	# 			os.path.join('maps', 'bw_maps', map2),
-	# 			path1,
-	# 			path2
-	# 		)
-	#
-	# 		imgs[0].save(filename1)
-	# 		imgs[1].save(filename2)
-	#
-	# 	kwargs = {
-	# 		'start_bldg': name,
-	# 		'start_room': session['start-room'],
-	# 		'dest_bldg': name,
-	# 		'dest_room': session['end-room'],
-	# 		'bldg_maps': [filename1, filename2],
-	# 		'gmaps': False
-	# 	}
-	# 	return render_template('result.html', **kwargs)
-	# else:
-	# 	img_filenames = []
-	#
-	# 	# different buildings
-	# 	start_name = translation[start_bldg]['building_name']
-	# 	start_data = os.path.join('maps', 'map_data', translation[start_bldg]['map_data'])
-	# 	start_graph = Graph.from_file(start_data)
-	# 	start = graph.get_from_label(session['start-room'])
-	#
-	# 	# find closest exit
-	# 	exit_nodes = [n for n in start_graph.nodes if n.label == 'EXIT']
-	#
-	# 	# sorted list, according to cost: [(node, cost, path)]
-	# 	exit, cost, path = sorted([(e, *graph.dijkstra(start, e)) for e in exit_nodes], key=lambda t: t[1])[0]
-	#
-	#
-	#
-	# 	z0, z1 = start.z, exit.z
-	#
-	# 	map1 = translation[start_bldg]['bw_maps'][str(start.z)]
-	# 	map2 = translation[start_bldg]['bw_maps'][str(exit.z)]
-	#
-	# 	filename1 = os.path.join('static', f'{map1}_{t}.png')
-	# 	filename2 = os.path.join('static', f'{map2}_{t}.png')
-	#
-	# 	img1, img2 = genimg3D(
-	# 		os.path.join('maps', 'bw_maps', map1),
-	# 		os.path.join('maps', 'bw_maps', map2),
-	# 		list(filter(lambda n: n.z == start.z, path)),
-	# 		list(filter(lambda n: n.z == exit.z, path))
-	# 	)
-	# 	img1.save(filename1)
-	# 	img2.save(filename2)
-	#
-	# 	img_filenames = [filename1, filename2]
-	#
-	#
-	# 	end_name = translation[end_bldg]['building_name']
-	# 	end_data = os.path.join('maps', 'map_data', translation[end_bldg]['map_data'])
-	# 	end_graph = Graph.from_file(end_data)
-	# 	end_end = graph.get_from_label(session['end-room'])
-	#
-	# 	kwargs = {
-	# 		'start_bldg': start_name,
-	# 		'start_room': session['start-room'],
-	# 		'dest_bldg': end_bldg,
-	# 		'dest_room': session['end-room'],
-	# 		'gmaps': True,
-	# 		'bldg_maps': img_filenames
-	# 	}
 
 @app.after_request
 def add_header(response):
@@ -179,5 +74,5 @@ def add_header(response):
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	# app.run(host='0.0.0.0', port=port)
-	app.run(host='127.0.0.1', port=5000)
+	app.run(host='0.0.0.0', port=port)
+	# app.run(host='127.0.0.1', port=5000)  # localhost
